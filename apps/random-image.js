36036e5b10947e0a90dc2c6cdfb430e252c8275d
Vladimir Kraykin <kraykin93@gmail.com>
2020-10-02 18:19:53 +0300

    :octocat:

--- apps/random-image.js
+++ apps/random-image.js
@@ -1,12 +1,26 @@
-const randomCatUrl = require('random-cat')
+const axios = require('axios')
 const { memeAsync } = require('memejs')
 
-const sendRandomCat = ({ replyWithPhoto }) => replyWithPhoto(randomCatUrl.get());
-const sendRandomMeme = async ({ replyWithPhoto }) => replyWithPhoto(await memeAsync())
+const sendRandomCat = async ({ replyWithPhoto }) => {
+  try {
+    const { data } = await axios.get('https://api.thecatapi.com/v1/images/search')
+    replyWithPhoto({ url: data[0].url })
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+const sendRandomMeme = async ({ replyWithPhoto }) => {
+  try {
+    replyWithPhoto(await memeAsync())
+  } catch (e) {
+    console.error(e)
+  }
+}
 
 module.exports = {
   enable(bot) {
     bot.command('cat', sendRandomCat)
     bot.command('meme', sendRandomMeme)
-  },
+  }
 }
