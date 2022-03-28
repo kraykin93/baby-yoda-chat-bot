09a224a8d1ddd7c3b8f3d33e3e216f33e09c066b
vkraykin <fi://1984.GHT0>
2022-03-28 20:11:48 +0200

    - add some offencive replies

--- apps/offence.js
+++ apps/offence.js
@@ -7,11 +7,14 @@ module.exports = {
     bot.hears(/птн/i, (ctx) => {
       ctx.reply('пнх', { reply_to_message_id: ctx.message.message_id });
     })
+    bot.hears(/путин/i, (ctx) => {
+      ctx.reply('хуйло', { reply_to_message_id: ctx.message.message_id });
+    })
     bot.hears(/феделеш/i, (ctx) => {
       ctx.reply('лох', { reply_to_message_id: ctx.message.message_id });
     })
-    bot.hears(/путин/i, (ctx) => {
-      ctx.reply('хуйло', { reply_to_message_id: ctx.message.message_id });
+    bot.hears(/артем/i, (ctx) => {
+      ctx.reply('Арсен', { reply_to_message_id: ctx.message.message_id });
     })
   }
 }
