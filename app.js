bd3501c8d3ca895aa7ef13e68598b596b47a5286
vkraykin <fi://1984.GHT0>
2022-04-09 11:13:01 +0200

    - add some offencive replies

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const { Telegraf } = require('telegraf')
 const randomImageApp = require('./apps/random-image')
 const offence = require('./apps/offence')
 
-const bot = new Telegraf('1381834875:AAE_eaRTtFLCOQlPrzBkJVetGvg75ZKJHYQ')
-bot.start((ctx) => ctx.reply('May the Force be with you!'))
+const bot = new Telegraf(process.env.BOT_TOKEN)
+bot.start(({ reply }) => reply('May the Force be with you!'))
 
 bot.on('new_chat_members', (ctx) => {
   ctx.message.new_chat_members.forEach((member) => {
