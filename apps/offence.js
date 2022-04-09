8c76c549a950547aa14fa036c50580c58ce3b8e5
vkraykin <fi://1984.GHT0>
2022-04-09 13:42:48 +0200

    - add some offencive replies

--- apps/offence.js
+++ apps/offence.js
@@ -1,21 +1,31 @@
+const { client: db } = require('../db/db')
+const CONST = require('../conf/CONST')
+
 module.exports = {
   enable(bot) {
     bot.hears(/птн/i, (ctx) => {
-      ctx.reply('пнх', { reply_to_message_id: ctx.message.message_id });
+      ctx.reply('пнх', { reply_to_message_id: ctx.message.message_id })
     })
     bot.hears(/путин/i, (ctx) => {
-      ctx.reply('путин хуйло!\nла-ла-ла-ла-ла-ла-ла-ла!', { reply_to_message_id: ctx.message.message_id });
+      ctx.reply('путин хуйло!\nла-ла-ла-ла-ла-ла-ла-ла!', { reply_to_message_id: ctx.message.message_id })
     })
     bot.hears(/жириновский/i, (ctx) => {
-      ctx.reply('земля стекловатой', { reply_to_message_id: ctx.message.message_id });
+      ctx.reply('земля стекловатой', { reply_to_message_id: ctx.message.message_id })
     })
     bot.hears(/кива/i, (ctx) => {
-      ctx.reply('уебан', { reply_to_message_id: ctx.message.message_id });
+      ctx.reply('уебан', { reply_to_message_id: ctx.message.message_id })
     })
-    bot.hears(/кто пидар/i, (ctx) => {
-      const names = ['феделеш', 'саня', 'артем', 'арсен', 'юра', 'толя', 'край']
-      const i = Math.floor(Math.random() * names.length);
-      ctx.reply(names[i], { reply_to_message_id: ctx.message.message_id });
+    bot.hears(/кто пидар/i, async (ctx) => {
+      const names = Object.values(CONST)
+      const i = Math.floor(Math.random() * names.length)
+      await db.query(`UPDATE public.rating SET counter = counter + 1 WHERE name = '${names[i].record}';`)
+      ctx.reply(names[i].name, { reply_to_message_id: ctx.message.message_id })
+    })
+    bot.command('pidorating', async (ctx) => {
+      const res = await db.query('SELECT * FROM public.rating;')
+      const map = new Map(res.rows.map(i => [i.name, i.counter]));
+      const msg = `${CONST.fedelesh.name}: ${map.get(CONST.fedelesh.record)}\n${CONST.sanya.name}: ${map.get(CONST.sanya.record)}\n${CONST.artem.name}: ${map.get(CONST.artem.record)}\n${CONST.yura.name}: ${map.get(CONST.yura.record)}\n${CONST.tolya.name}: ${map.get(CONST.tolya.record)}\n${CONST.kray.name}: ${map.get(CONST.kray.record)}`
+      ctx.reply(msg)
     })
   }
 }
---
+++ conf/CONST.js
@@ -0,0 +1,26 @@
+module.exports = {
+  fedelesh: {
+    name: 'феделеш',
+    record: 'fedelesh',
+  },
+  sanya: {
+    name: 'саня',
+    record: 'sanya',
+  },
+  artem: {
+    name: 'артем',
+    record: 'artem',
+  },
+  yura: {
+    name: 'юра',
+    record: 'yura',
+  },
+  tolya: {
+    name: 'толя',
+    record: 'tolya',
+  },
+  kray: {
+    name: 'край',
+    record: 'kray',
+  },
+}
