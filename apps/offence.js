2cba45e6b27ec19dc772be172ec056f2a4fbdd33
vkraykin <fi://1984.GHT0>
2022-04-10 13:03:33 +0200

    - add some offencive replies

--- apps/offence.js
+++ apps/offence.js
@@ -1,5 +1,4 @@
 const { client: db } = require('../db/db');
-const CONST = require('../conf/CONST');
 
 module.exports = {
   enable(bot) {
@@ -12,19 +11,21 @@ module.exports = {
     bot.hears(/жириновский/i, (ctx) => {
       ctx.reply('земля стекловатой', { reply_to_message_id: ctx.message.message_id });
     });
+    bot.hears(/лукашенко/i, (ctx) => {
+      ctx.reply('А я сейчас вам покажу, откуда на Беларусь готовилось нападение...', { reply_to_message_id: ctx.message.message_id });
+    });
     bot.hears(/кива/i, (ctx) => {
       ctx.reply('уебан', { reply_to_message_id: ctx.message.message_id });
     });
     bot.hears(/кто пидар/i, async (ctx) => {
-      const names = Object.values(CONST);
+      const { rows: names } = await db.query('SELECT * FROM public.rating;');
       const i = Math.floor(Math.random() * names.length);
-      await db.query(`UPDATE public.rating SET counter = counter + 1 WHERE name = '${names[i].record}';`);
+      await db.query(`UPDATE public.rating SET counter = counter + 1 WHERE name = '${names[i].name}';`);
       ctx.reply(names[i].name, { reply_to_message_id: ctx.message.message_id });
     });
     bot.command('pidorating', async (ctx) => {
-      const res = await db.query('SELECT * FROM public.rating;');
-      const map = new Map(res.rows.map(i => [i.name, i.counter]));
-      const msg = `${CONST.fedelesh.name}: ${map.get(CONST.fedelesh.record)}\n${CONST.sanya.name}: ${map.get(CONST.sanya.record)}\n${CONST.artem.name}: ${map.get(CONST.artem.record)}\n${CONST.yura.name}: ${map.get(CONST.yura.record)}\n${CONST.tolya.name}: ${map.get(CONST.tolya.record)}\n${CONST.kray.name}: ${map.get(CONST.kray.record)}`;
+      const { rows: names } = await db.query('SELECT * FROM public.rating;');
+      const msg = names.reduce((prev, current) => prev.concat('\n', `${current.name}: ${current.counter}`), '')
       ctx.reply(msg);
     });
   },
