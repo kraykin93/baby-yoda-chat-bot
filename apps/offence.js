44eb0ab6e354084a218969b1cb36051a22509615
vkraykin <fi://1984.GHT0>
2022-04-15 12:33:37 +0200

    - add some offencive replies

--- apps/offence.js
+++ apps/offence.js
@@ -12,8 +12,11 @@ module.exports = {
     bot.hears(/песков/i, (ctx) => {
       ctx.reply('этот ебанат совсем запизделся', { reply_to_message_id: ctx.message.message_id });
     });
+    bot.hears(/медведев/i, (ctx) => {
+      ctx.reply('денег нет! но вы (терпилы) держитесь!', { reply_to_message_id: ctx.message.message_id });
+    });
     bot.hears(/лукашенко/i, (ctx) => {
-      ctx.reply('А я сейчас вам покажу, откуда на Беларусь готовилось нападение...', { reply_to_message_id: ctx.message.message_id });
+      ctx.replyWithPhoto({ source: 'assets/img/usach.png' }, { reply_to_message_id: ctx.message.message_id });
     });
     bot.hears(/кива/i, (ctx) => {
       ctx.reply('уебан', { reply_to_message_id: ctx.message.message_id });
