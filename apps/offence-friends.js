fcdc5cf695388a881aef62f668e3630d6ed14052
vkraykin <fi://1984.GHT0>
2022-04-17 18:00:23 +0200

    - add some offencive replies

--- apps/offence-friends.js
+++ apps/offence-friends.js
@@ -4,26 +4,26 @@ module.exports = {
   enable(bot) {
     bot.hears(/феделеш/i, async (ctx) => {
       await db.query(`UPDATE public.rating SET counter = counter + 1 WHERE name = 'феделеш';`);
-      ctx.reply('пидор (+1)', { reply_to_message_id: ctx.message.message_id });
+      ctx.reply('ф пидор (+1)', { reply_to_message_id: ctx.message.message_id });
     });
     bot.hears(/артем|артём/i, (ctx) => {
-      const msgs = ['хуй', 'арсен'];
+      const msgs = ['артем хуй', 'арсен'];
       const i = Math.floor(Math.random() * msgs.length);
       ctx.reply(msgs[i], { reply_to_message_id: ctx.message.message_id });
     });
     bot.hears(/саня|саша|харина/i, (ctx) => {
-      const msgs = ['хуй сАси', 'а теперь танцуй'];
+      const msgs = ['саня хуй сАси', 'саня а теперь танцуй'];
       const i = Math.floor(Math.random() * msgs.length);
       ctx.reply(msgs[i], { reply_to_message_id: ctx.message.message_id });
     });
     bot.hears(/юра/i, (ctx) => {
-      ctx.reply('красава ветеран', { reply_to_message_id: ctx.message.message_id });
+      ctx.reply('/юра красава ветеран', { reply_to_message_id: ctx.message.message_id });
     });
     bot.hears(/толя/i, (ctx) => {
-      ctx.reply('красава капитан', { reply_to_message_id: ctx.message.message_id });
+      ctx.reply('толя красава капитан', { reply_to_message_id: ctx.message.message_id });
     });
     bot.hears(/край/i, (ctx) => {
-      const msgs = ['хуй вонючий', 'феделеш, или на хуй'];
+      const msgs = ['край хуй вонючий', 'феделеш, иди на хуй'];
       const i = Math.floor(Math.random() * msgs.length);
       ctx.reply(msgs[i], { reply_to_message_id: ctx.message.message_id });
     });
