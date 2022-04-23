27ac61e7cd52784f0a3f35ee0bff014a99aaca62
vkraykin <fi://1984.GHT0>
2022-04-23 14:12:47 +0200

    - add some offencive replies

--- apps/offence.js
+++ apps/offence.js
@@ -3,23 +3,26 @@ module.exports = {
     bot.hears(/птн/i, (ctx) => {
       ctx.reply('пнх', { reply_to_message_id: ctx.message.message_id });
     });
-    bot.hears(/путин/i, (ctx) => {
-      ctx.reply('путин хуйло!\nла-ла-ла-ла-ла-ла-ла-ла!', { reply_to_message_id: ctx.message.message_id });
-    });
-    bot.hears(/жириновский/i, (ctx) => {
-      ctx.reply('земля стекловатой', { reply_to_message_id: ctx.message.message_id });
-    });
-    bot.hears(/песков/i, (ctx) => {
-      ctx.reply('этот ебанат совсем запизделся', { reply_to_message_id: ctx.message.message_id });
-    });
-    bot.hears(/медведев/i, (ctx) => {
-      ctx.reply('денег нет! но вы держитесь!', { reply_to_message_id: ctx.message.message_id });
-    });
-    bot.hears(/лукашенко/i, (ctx) => {
-      ctx.replyWithPhoto({ source: 'assets/img/usach.png' }, { reply_to_message_id: ctx.message.message_id });
-    });
-    bot.hears(/кива/i, (ctx) => {
-      ctx.reply('кива уебан', { reply_to_message_id: ctx.message.message_id });
+    bot.on(['text', 'caption'], (ctx) => {
+      const text = ctx.message.text || ctx.message.text;
+      if (/путин/i.test(text)) {
+        ctx.reply('путин хуйло!\nла-ла-ла-ла-ла-ла-ла-ла!', { reply_to_message_id: ctx.message.message_id });
+      }
+      if (/медведев/i.test(text)) {
+        ctx.reply('денег нет! но вы держитесь!', { reply_to_message_id: ctx.message.message_id });
+      }
+      if (/песков/i.test(text)) {
+        ctx.reply('песков ебанат уже совсем запизделся', { reply_to_message_id: ctx.message.message_id });
+      }
+      if (/жириновский/i.test(text)) {
+        ctx.reply('земля стекловатой, жирик', { reply_to_message_id: ctx.message.message_id });
+      }
+      if (/лукашенко/i.test(text)) {
+        ctx.replyWithPhoto({ source: 'assets/img/usach.png' }, { reply_to_message_id: ctx.message.message_id });
+      }
+      if (/кива/i.test(text)) {
+        ctx.reply('кива уебан', { reply_to_message_id: ctx.message.message_id });
+      }
     });
   },
 };
