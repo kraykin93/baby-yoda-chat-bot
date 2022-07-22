81f0dd61a2d468316810673550e4be05a8368856
vkraykin <Liveperson1>
2022-07-22 09:19:25 +0300

    add new command last***

--- apps/pidorating.js
+++ apps/pidorating.js
@@ -5,6 +5,7 @@ async function onWhoPidor(ctx) {
   const { rows: names } = await db.query('SELECT * FROM public.rating;');
   const i = chance.integer({ min: 0, max: names.length - 1 });
   await db.query(`UPDATE public.rating SET counter = counter + 1 WHERE name = '${names[i].name}';`);
+  await db.query(`UPDATE public.last SET last = '${names[i].name}';`);
   ctx.reply(names[i].name, { reply_to_message_id: ctx.message.message_id });
 }
 
@@ -16,7 +17,13 @@ async function onPidorating(ctx) {
   ctx.reply(msg);
 }
 
+async function onLastPidor(ctx) {
+  const { rows } = await db.query('SELECT * FROM public.last;');
+  ctx.reply(`последним пидором удостоен быть *${rows[0].last}*`, { parse_mode: "MarkdownV2" });
+}
+
 module.exports = (bot) => {
   bot.hears(/кто пидар|кто пидор/i, onWhoPidor);
   bot.command('pidorating', onPidorating);
+  bot.command('lastpidor', onLastPidor);
 };
