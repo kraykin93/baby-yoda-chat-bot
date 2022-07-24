50b37a1f526cdb154383a134b91ff3927122010f
vkraykin <Liveperson1>
2022-07-24 22:40:39 +0300

    add new features

--- apps/pidorating.js
+++ apps/pidorating.js
@@ -11,7 +11,7 @@ async function onWhoPidor(ctx) {
   ctx.reply(persons[i].name, { reply_to_message_id: ctx.message.message_id });
 }
 
-async function onPidorating(ctx) {
+async function onPidorRating(ctx) {
   const { rows: persons } = await db.query('SELECT * FROM public.rating;');
   const msg = persons
     .sort((a, b) => b.counter - a.counter)
@@ -19,7 +19,8 @@ async function onPidorating(ctx) {
   ctx.reply(msg);
 }
 
-async function onPidoduration(ctx) {
+async function onPidorDuration(ctx) {
+  await updateLastPidorDuration();
   const { rows: persons } = await db.query('SELECT * FROM public.rating;');
   const msg = persons
     .sort((a, b) => b.duration - a.duration)
@@ -27,19 +28,27 @@ async function onPidoduration(ctx) {
   ctx.reply(msg);
 }
 
-async function onLastPidor(ctx) {
+async function updateLastPidorDuration() {
   const { rows } = await db.query('SELECT * FROM public.last;');
   const { rows: duration } = await db.query(`SELECT duration FROM public.rating WHERE name = '${rows[0].name}';`);
   const diff = Date.now() - Number(rows[0].date);
+
   if (duration[0].duration < diff) {
     await db.query(`UPDATE public.rating SET duration = ${diff} WHERE name = '${rows[0].name}';`);
   }
-  ctx.reply(`*${rows[0].name}* удостоен быть пидором уже${parseDays(diff)}`, { parse_mode: 'MarkdownV2' });
+
+  return { name: rows[0].name, diff };
+}
+
+async function onLastPidor(ctx) {
+  const { name, diff } = await updateLastPidorDuration();
+  console.log({ name, diff });
+  ctx.reply(`*${name}* удостоен быть пидором уже${parseDays(diff)}`, { parse_mode: 'MarkdownV2' });
 }
 
 module.exports = (bot) => {
   bot.hears(/кто пидар|кто пидор|кто пидрила|кто пидорас/i, onWhoPidor);
-  bot.command('pidorating', onPidorating);
-  bot.command('pidoduration', onPidoduration);
+  bot.command('pidorating', onPidorRating);
+  bot.command('pidoduration', onPidorDuration);
   bot.command('lastpidor', onLastPidor);
 };
