dce2223a823e88abae7d59b816a09c5ff8c568e1
vkraykin <Liveperson1>
2022-07-24 20:54:32 +0300

    add new features

--- apps/pidorating.js
+++ apps/pidorating.js
@@ -1,3 +1,4 @@
+const moment = require('moment');
 const chance = require('chance').Chance();
 const { client: db } = require('../db/db');
 
@@ -5,7 +6,7 @@ async function onWhoPidor(ctx) {
   const { rows: names } = await db.query('SELECT * FROM public.rating;');
   const i = chance.integer({ min: 0, max: names.length - 1 });
   await db.query(`UPDATE public.rating SET counter = counter + 1 WHERE name = '${names[i].name}';`);
-  await db.query(`UPDATE public.last SET last = '${names[i].name}';`);
+  await db.query(`UPDATE public.last SET name = '${names[i].name}', date = '${Date.now()}';`);
   ctx.reply(names[i].name, { reply_to_message_id: ctx.message.message_id });
 }
 
@@ -19,7 +20,8 @@ async function onPidorating(ctx) {
 
 async function onLastPidor(ctx) {
   const { rows } = await db.query('SELECT * FROM public.last;');
-  ctx.reply(`последним пидором удостоен быть *${rows[0].last}*`, { parse_mode: "MarkdownV2" });
+  const diff = new moment.duration(Date.now() - Number(rows[0].date));
+  ctx.reply(`*${rows[0].name}* удостоен быть пидором уже ${Math.floor(diff.asDays())} дней, ${Math.floor(diff.asHours())} часов, ${Math.floor(diff.asMinutes())} минут`, { parse_mode: "MarkdownV2" });
 }
 
 module.exports = (bot) => {
